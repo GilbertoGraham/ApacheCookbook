438eae9f4ce67483dfd0e2de8dd30354d3ce2c67
root <root@mnhqfelt15vm01.ltfinc.net>
2018-02-22 20:00:38 +0000

    add remote file and modified the index template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,6 +2,10 @@ package 'httpd' do
 	action :install
 end
 
+remote_file 'var/www/html/yoda-bong.jpg' do
+	source 'https://serpenturbanclothing.com/wp-content/uploads/2018/02/yoda-bong.jpg'
+end
+
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
 	variables(
